3feb3ba9db2bdfe9efaca156213bce804da84311
“yvetteni” <yvetteni@student.ubc.ca>
2025-11-19 23:54:17 -0800

    fixing error

--- R/descstat.R
+++ R/descstat.R
@@ -15,25 +15,20 @@
 #' @export
 #'
 
-library(roxygen2) #describe new function
-library(testthat) #verify new function
-library(tidyverse) #package used by new function
-library(dplyr) #package used by new function
-library(gapminder) #dataset used by demonstration
-
+library(magrittr)
 descstat <- function(data, group, numeric_variable, na.rm = TRUE) {
 
   needs_to_be_numeric <- data %>%
-    select ({{numeric_variable}}) %>%
-    pull () #this is to extract the numeric column
+    dplyr::select ({{numeric_variable}}) %>%
+    dplyr::pull () #this is to extract the numeric column
 
   if (!is.numeric(needs_to_be_numeric)) {
     stop("numeric_variable must contain numeric values, duh")
   } #this will stop the function if they input incorrectly
 
   descriptive_data <- data %>%
-    group_by({{group}}) %>%
-    summarise(min = min({{numeric_variable}},na.rm = na.rm),
+    dplyr::group_by({{group}}) %>%
+    dplyr::summarise(min = min({{numeric_variable}},na.rm = na.rm),
               max = max({{numeric_variable}}, na.rm = na.rm),
               mean = mean({{numeric_variable}}, na.rm = na.rm),
               median = median({{numeric_variable}}, na.rm = na.rm),
